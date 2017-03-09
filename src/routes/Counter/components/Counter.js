02930879b7210b984b3d23c9aaeaddee7600f443
Ренат Саматов <awakentwice@gmail.com>
2017-03-09 22:05:54 +0300

    chore: add @blueprints to repo

--- src/routes/Counter/components/Counter.js
+++ src/routes/Counter/components/Counter.js
@@ -3,13 +3,16 @@ import React from 'react'
 export const Counter = (props) => (
   <div style={{ margin: '0 auto' }} >
     <h2>Counter: {props.counter}</h2>
-    <button className='btn btn-default' onClick={props.increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-default' onClick={props.doubleAsync}>
-      Double (Async)
-    </button>
+
+    <div className="pt-button-group">
+      <button className='pt-button pt-icon-database' onClick={props.increment}>
+        Increment
+      </button>
+      {' '}
+      <button className='pt-button pt-icon-function' onClick={props.doubleAsync}>
+        Double (Async)
+      </button>
+    </div>
   </div>
 )
 
